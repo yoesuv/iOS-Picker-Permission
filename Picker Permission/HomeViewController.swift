78a412c0837c1d861492b0f2c8ecd26ec2922083
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-04 20:22:43 +0700

    :art: setup navigation bar appearance

--- Picker Permission/HomeViewController.swift
+++ Picker Permission/HomeViewController.swift
@@ -11,9 +11,20 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        self.title = "Picker & Permissions"
         self.navigationItem.backButtonDisplayMode = .minimal
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "AccentColor")!
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        self.navigationController?.navigationBar.standardAppearance  = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationController?.navigationBar.compactAppearance = appearance
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.tintColor = .white
     }
 
 
