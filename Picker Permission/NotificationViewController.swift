337cec1c12fd73fb71428bd24ca0dedf2e292b31
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-04 20:59:42 +0700

    :art: UI notification

--- Picker Permission/NotificationViewController.swift
+++ Picker Permission/NotificationViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class NotificationViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelPermission: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Notification"
     }
 
+    @IBAction func clickLocalNotification(_ sender: UIButton) {
+        labelPermission.text = "Check Permission Status"
+    }
 }
