ebdb9d53468819a287d1755b807f43b25403e458
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-06 14:03:54 +0700

    :sparkles: pick image from gallery

--- Picker Permission/GalleryViewController.swift
+++ Picker Permission/GalleryViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI
 
 class GalleryViewController: UIViewController {
     
@@ -19,7 +20,39 @@ class GalleryViewController: UIViewController {
     }
     
     @IBAction func clickOpenGallery(_ sender: UIButton) {
-        
+        var phPickerConfig = PHPickerConfiguration(photoLibrary: .shared())
+        phPickerConfig.selectionLimit = 1
+        phPickerConfig.filter = PHPickerFilter.any(of: [.images, .livePhotos])
+        let phPickerVC = PHPickerViewController(configuration: phPickerConfig)
+        phPickerVC.delegate = self
+        present(phPickerVC, animated: true)
+    }
+    
+}
+
+extension GalleryViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: .none)
+        if let result = results.first {
+            // load image
+            result.itemProvider.loadObject(ofClass: UIImage.self) { reading, error in
+                if error == nil {
+                    guard let image = reading as? UIImage else { return }
+                    DispatchQueue.main.async {
+                        self.imageViewGallery.image = image
+                    }
+                }
+            }
+            // load path
+            result.itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.image.identifier) { url, error in
+                if (error == nil) {
+                    DispatchQueue.main.async {
+                        self.labelPath.text = url?.absoluteString
+                    }
+                }
+            }
+        }
     }
     
 }
