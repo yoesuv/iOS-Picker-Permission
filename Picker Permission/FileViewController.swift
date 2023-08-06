b484c7954bd2e09a86c2d7004add82893d1f1063
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-06 14:20:44 +0700

    :art: UI picker file

--- Picker Permission/FileViewController.swift
+++ Picker Permission/FileViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class FileViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelFilePath: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "File"
+        self.labelFilePath.text = ""
     }
-
+    
+    @IBAction func clickOpenFile(_ sender: UIButton) {
+        
+    }
+    
 }
