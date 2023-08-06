37e94a71bbb61da670d68418ca5c1a0d5661051c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-06 13:17:14 +0700

    :art: UI picker gallery

--- Picker Permission/GalleryViewController.swift
+++ Picker Permission/GalleryViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class GalleryViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewGallery: UIImageView!
+    @IBOutlet weak var labelPath: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Gallery"
+        self.labelPath.text = ""
     }
-
+    
+    @IBAction func clickOpenGallery(_ sender: UIButton) {
+        
+    }
+    
 }
