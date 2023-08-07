64519d462c0de3917b651825aab7adaf79bc2191
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-07 11:44:51 +0700

    :art: UI picker location

--- Picker Permission/LocationViewController.swift
+++ Picker Permission/LocationViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class LocationViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelLatLng: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Location"
+        self.labelLatLng.text = ""
     }
-
+    
+    
+    @IBAction func clickGetLocation(_ sender: UIButton) {
+        
+    }
+    
 }
