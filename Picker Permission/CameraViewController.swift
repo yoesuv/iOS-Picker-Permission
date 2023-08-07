9a3044c99575315d2e4fc4b329f2f4e73d82281b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-07 16:18:25 +0700

    :pencil: camera permission

--- Picker Permission/CameraViewController.swift
+++ Picker Permission/CameraViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
     
@@ -19,7 +20,28 @@ class CameraViewController: UIViewController {
     }
     
     @IBAction func clickOpenCamera(_ sender: UIButton) {
-        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            print("Not Determined")
+            AVCaptureDevice.requestAccess(for: .video) { access in
+                print("CameraViewController # access \(access)")
+                if access {
+                    self.openCamera()
+                }
+            }
+        case .restricted:
+            print("Restricted")
+        case .denied:
+            print("Denied")
+        case .authorized:
+            self.openCamera()
+        default:
+            print("Default")
+        }
+    }
+    
+    private func openCamera() {
+        print("CameraViewController # open camera")
     }
     
 }
