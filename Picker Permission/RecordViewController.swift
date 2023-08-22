a60ea5277a775246124b565a9d426efffc821a0e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-22 21:07:54 +0700

    :sparkles: record & play

--- Picker Permission/RecordViewController.swift
+++ Picker Permission/RecordViewController.swift
@@ -13,6 +13,12 @@ class RecordViewController: UIViewController {
     @IBOutlet weak var labelRecordingState: UILabel!
     @IBOutlet weak var labelRunning: UILabel!
     
+    private var recordingSession: AVAudioSession!
+    private var audioRecorder: AVAudioRecorder!
+    private var player: AVAudioPlayer!
+    private var audioFileName: URL!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Record Audio"
@@ -37,11 +43,46 @@ class RecordViewController: UIViewController {
     }
     
     @IBAction func onClickStop(_ sender: UIButton) {
-        print("RecordViewController # click STOP")
+        self.audioRecorder.stop()
+        self.audioRecorder = nil
     }
     
     private func startOrPauseRecording() {
         print("RecordViewController # Start or Pause Recording")
+        let fileManager = FileManager.default
+        let dirPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        self.audioFileName = dirPath[0].appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.medium.rawValue
+        ]
+        do {
+            self.audioRecorder = try AVAudioRecorder(url: audioFileName, settings: settings)
+            self.audioRecorder.delegate = self
+            self.audioRecorder.record()
+        } catch {
+            print("RecordViewController # error start recording \(error)")
+        }
+    }
+    
+}
+
+extension RecordViewController: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            do {
+                try player = AVAudioPlayer(contentsOf: audioFileName, fileTypeHint: AVFileType.m4a.rawValue)
+                player.play()
+            } catch let error {
+                print("RecordViewController # error \(error.localizedDescription)")
+            }
+        } else {
+            print("RecordViewController # error play")
+        }
     }
     
 }
