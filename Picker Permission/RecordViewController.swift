1c4838a15b93853a912479b3aac4f400b3b339a0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-22 14:52:01 +0700

    :art: UI recording audio

--- Picker Permission/RecordViewController.swift
+++ Picker Permission/RecordViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class RecordViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelRecordingState: UILabel!
+    @IBOutlet weak var labelRunning: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Record Audio"
     }
-
+    
+    
+    @IBAction func onClickStart(_ sender: UIButton) {
+        print("RecordViewController # click START")
+    }
+    
+    @IBAction func onClickStop(_ sender: UIButton) {
+        print("RecordViewController # click STOP")
+    }
+    
 }
